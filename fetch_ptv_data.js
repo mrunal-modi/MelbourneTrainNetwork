b85daca6a0a782a6185cba57482b9cd114249413
mmodi <mrunal.modi@gmail.com>
2022-01-26 12:21:06 +1100

    trying to understand promise async await and store json into variable

--- fetch_ptv_data.js
+++ fetch_ptv_data.js
@@ -1,11 +1,9 @@
 (async () => {
     try {
-      const res = await fetch('http://timetableapi.ptv.vic.gov.au/v3/stops/route/1/route_type/0?devid=3002044&signature=57CEFA51CD688E6D1B0833389E50A2371DEE3CA2')
-      const out = await res.json()
-
-      const stops = out.stops;
+      const response = await fetch('http://timetableapi.ptv.vic.gov.au/v3/stops/route/1/route_type/0?devid=3002044&signature=57CEFA51CD688E6D1B0833389E50A2371DEE3CA2');
+      const data = await response.json();
+      const stops = data.stops;
       const len = stops.length;
-
       let stop_names = '';
       let geoLocation = '';
       for (let i = 0; i < len; i++) {
