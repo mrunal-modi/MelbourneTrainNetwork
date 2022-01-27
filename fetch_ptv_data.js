bcaa7363ca890d2bdedfc466bba405c5c4af4526
mrunal-modi <mrunal.modi@gmail.com>
2022-01-27 20:17:41 +1100

    fetchDatafromURL update

--- fetch_ptv_data.js
+++ fetch_ptv_data.js
@@ -1,19 +1,30 @@
-(async () => {
-    try {
-      const response = await fetch('http://timetableapi.ptv.vic.gov.au/v3/stops/route/1/route_type/0?devid=3002044&signature=57CEFA51CD688E6D1B0833389E50A2371DEE3CA2');
-      const data = await response.json();
-      const stops = data.stops;
-      const len = stops.length;
-      let stop_names = '';
-      let geoLocation = '';
-      for (let i = 0; i < len; i++) {
-          stop_names += stops[i]['stop_name'];
-          geoLocation += "[" + stops[i]['stop_longitude'] +", " + stops[i]['stop_latitude'] + "],";
-        }
-        console.log(stop_names);
-        console.log(geoLocation);
+// (async () => {
+//     try {
+//       const response = await fetch('http://timetableapi.ptv.vic.gov.au/v3/stops/route/1/route_type/0?devid=3002044&signature=57CEFA51CD688E6D1B0833389E50A2371DEE3CA2');
+//       const data = await response.json();
+//       const stops = data.stops;
+//       const len = stops.length;
+//       let stop_names = '';
+//       let geoLocation = '';
+//       for (let i = 0; i < len; i++) {
+//           stop_names += stops[i]['stop_name'];
+//           geoLocation += "[" + stops[i]['stop_longitude'] +", " + stops[i]['stop_latitude'] + "],";
+//         }
+//         console.log(stop_names);
+//         console.log(geoLocation);
 
-    } catch (err) {
-      throw err
-    }
-  })()
+//     } catch (err) {
+//       throw err
+//     }
+//   })()
+
+
+  async function fetchDataFromURL() {
+    let url = 'http://timetableapi.ptv.vic.gov.au/v3/stops/route/1/route_type/0?devid=3002044&signature=57CEFA51CD688E6D1B0833389E50A2371DEE3CA2';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }
+
+  fetchDataFromURL();
